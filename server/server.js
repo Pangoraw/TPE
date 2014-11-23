c81e1202e3e27884afbae3fe7e9e5229e2aa016a
Pangoraw <naydex.mc@gmail.com>
2014-11-23 20:59:38 +0100

    Biggest Update.
    We made it work. It contains the node_module folder, the client and
    server files as well. And everything is working fine. If you want
    explanation, then look into the client folder, I made a picture to
    figure out how this awesome thing can be working.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,36 @@
 var app = require('express')(),
 	server = require('http').createServer(app),
-	fs = require('fs'),
-	ent = require('ent');
+	path = require('path'),
+	fs = require('fs');
 
 app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/index.html');
+	res.sendFile('index.html', { root: path.join(__dirname, '../client') });
 });
 
-io = require('socket.io').listen(server);
+var io = require('socket.io');
+var socket = io.listen(server);
 
-io.sockets.on('connection', function(socket, pseudo){
+socket.set("log level", 1);
 
+socket.on('connection', function (client) {
 
+	client.on('pseudo', function (pseudo) {
+		console.log( pseudo + ' logged in');
+		client.broadcast.emit('newConnection', pseudo);
+	});
+	client.on('message', function (data) {
+		client.broadcast.emit('message', {"pseudo": data.pseudo, "message": data.message});
+		socket.emit('message', {"pseudo": data.pseudo, "message": data.message});
+		console.log(data.pseudo + ' : ' + returnStr(data.message));
+	});
 });
+
+function returnStr (inputStr) {
+	var outputStr = '';
+	for (i = 0; i < inputStr.length; i++) {
+		outputStr = outputStr + inputStr[i];
+	}
+	return outputStr;
+}
 
 server.listen(8080, "0.0.0.0");
