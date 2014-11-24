b5091c25acc8b33c819b84eb6f1bf1174c24e50c
Pangoraw <naydex.mc@gmail.com>
2014-11-24 21:52:16 +0100

    Add of commentary.
    I added commentaries to help Théo to understand this code and whoever
    want to use it :)

--- server/server.js
+++ server/server.js
@@ -1,31 +1,30 @@
-var app = require('express')(),
+var app = require('express')(), // Masse import
 	server = require('http').createServer(app),
 	path = require('path'),
 	fs = require('fs');
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) { // Quand tu tapes localhost:8080/ il te renvoie index.html qui est situé dans le dossier client
 	res.sendFile('index.html', { root: path.join(__dirname, '../client') });
 });
 
-var io = require('socket.io');
+var io = require('socket.io'); // On defini le websocket
 var socket = io.listen(server);
 
-socket.set("log level", 1);
 
-socket.on('connection', function (client) {
+socket.on('connection', function (client) { // Chaque client a une valeur de client.id differente 
 
-	client.on('pseudo', function (pseudo) {
-		console.log( pseudo + ' logged in');
+	client.on('pseudo', function (pseudo) { // Quand il reçoit un pseudo il le renvoit à tout le monde (broadcast)
+		console.log( pseudo + ' logged in'); // Log dans l'invite de commande
 		client.broadcast.emit('newConnection', pseudo);
 	});
-	client.on('message', function (data) {
+	client.on('message', function (data) { // idem
 		client.broadcast.emit('message', {"pseudo": data.pseudo, "message": data.message});
 		socket.emit('message', {"pseudo": data.pseudo, "message": data.message});
 		console.log(data.pseudo + ' : ' + returnStr(data.message));
 	});
 });
 
-function returnStr (inputStr) {
+function returnStr (inputStr) { // Permet au serveur d'afficher la chaine codée sans virgule entre les caractères (parceque c'est un tableau)
 	var outputStr = '';
 	for (i = 0; i < inputStr.length; i++) {
 		outputStr = outputStr + inputStr[i];
@@ -33,4 +32,4 @@ function returnStr (inputStr) {
 	return outputStr;
 }
 
-server.listen(8080, "0.0.0.0");
+server.listen(8080, "0.0.0.0"); // Definie l'URL du serveur 
